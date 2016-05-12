954ceac95fd644dd8a3580c8030b74356bdc4e82
VarunBarve <varun7691@gmail.com>
2016-05-12 19:07:39 +0530

    API for attendance

--- app/src/main/java/varun/com/studentmanagementsystemsample/constants/Constants.java
+++ app/src/main/java/varun/com/studentmanagementsystemsample/constants/Constants.java
@@ -11,6 +11,7 @@ public class Constants {
     public static final String KEY_MESSAGE = "Message";
     public static final String KEY_OBJ_LOGIN_RESULT = "objLoginResult";
     public static final String KEY_OBJ_CHILD_RESULT = "objChildResult";
+    public static final String KEY_ATTENDANCE_DETAIL = "attendanceDetail";
     public static final String KEY_USER_ID = "userId";
     public static final String KEY_USER_NAME = "userName";
     public static final String KEY_PASSWORD = "password";
@@ -21,6 +22,9 @@ public class Constants {
     public static final String KEY_SCHOOL_ID = "schoolID";
     public static final String KEY_AVG_ATTENDANCE = "avrageAttendance";
     public static final String KEY_AVG_PERFORMANCE = "avragePerformance";
+    public static final String KEY_ATTENDANCE_RESULT = "AttendanceResult";
+    public static final String KEY_ATTENDANCE_DATE = "attendanceDate";
+    public static final String KEY_ATTENDANCE_STATUS = "attendanceStatus";
 
     //STATUS CODE DESC
     public static final int STATUS_CODE_SUCCESS = 1;
--- app/src/main/java/varun/com/studentmanagementsystemsample/fragments/AttendanceFragment.java
+++ app/src/main/java/varun/com/studentmanagementsystemsample/fragments/AttendanceFragment.java
@@ -1,8 +1,10 @@
 package varun.com.studentmanagementsystemsample.fragments;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,24 +13,50 @@ import android.widget.Toast;
 import com.roomorama.caldroid.CaldroidFragment;
 import com.roomorama.caldroid.CaldroidListener;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
 import varun.com.studentmanagementsystemsample.R;
+import varun.com.studentmanagementsystemsample.constants.Api;
+import varun.com.studentmanagementsystemsample.constants.Constants;
+import varun.com.studentmanagementsystemsample.utils.SessionManager;
 
 /**
  * Created by Varun on 4/2/2016.
  */
 public class AttendanceFragment extends Fragment {
+
+    InputStream inputStream;
+    StringBuilder stringBuilder;
+    String attendanceResponse;
+
+    SessionManager sessionManager;
+
     ArrayList<Date> disableDateList;
+
+    CaldroidFragment caldroidFragment;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        sessionManager = new SessionManager(AttendanceFragment.this.getActivity());
+
         View rootView = inflater.inflate(R.layout.fragment_attendance, container, false);
 
-        CaldroidFragment caldroidFragment = new CaldroidFragment();
+        caldroidFragment = new CaldroidFragment();
         Bundle args = new Bundle();
         Calendar cal = Calendar.getInstance();
         args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
@@ -36,7 +64,6 @@ public class AttendanceFragment extends Fragment {
         args.putInt(CaldroidFragment.START_DAY_OF_WEEK, CaldroidFragment.MONDAY);
         args.putBoolean(CaldroidFragment.ENABLE_CLICK_ON_DISABLED_DATES, false);
         args.putBoolean(CaldroidFragment.SQUARE_TEXT_VIEW_CELL, true);
-//        args.putInt(CaldroidFragment.THEME_RESOURCE, com.caldroid.R.style.CaldroidDefaultDark);
         caldroidFragment.setArguments(args);
 
         FragmentTransaction t = getActivity().getSupportFragmentManager().beginTransaction();
@@ -56,6 +83,111 @@ public class AttendanceFragment extends Fragment {
             }
         });
 
+        new ForAttendance().execute();
+
         return rootView;
+    }
+
+    class ForAttendance extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            JSONStringer childJsonStringer = new JSONStringer();
+
+            try {
+
+                childJsonStringer.object().key(Constants.KEY_USER_ID).value(sessionManager.getUserDetails().getUserId()).key(Constants.KEY_ATTENDANCE_DETAIL).value("").endObject();
+
+                URL url = new URL(Api.ATTENDANCE_URL);
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("charset", "utf-8");
+                conn.setRequestProperty("Content-type",
+                        "application/json");
+                conn.setRequestProperty("Accept",
+                        "application/json");
+
+                OutputStream os = conn.getOutputStream();
+
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+                        os, "UTF-8"));
+                writer.write(childJsonStringer.toString());
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+                inputStream = conn.getErrorStream();
+                if (inputStream == null) {
+                    inputStream = conn.getInputStream();
+                }
+
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(inputStream), 1000);
+                stringBuilder = new StringBuilder();
+                stringBuilder.append(reader.readLine() + "\n");
+
+                String line = "0";
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line + "\n");
+                }
+
+                inputStream.close();
+                attendanceResponse = stringBuilder.toString();
+
+            } catch (Exception e) {
+
+                Log.e("LOGIN ERROR: ", "LOGIN ERROR: " + e);
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            int userId = -1;
+
+            try {
+                JSONObject rootObject = new JSONObject(attendanceResponse);
+
+                int statusCode = rootObject.getInt(Constants.KEY_STATUS_CODE);
+
+                if (statusCode == Constants.STATUS_CODE_SUCCESS) {
+                    JSONArray attendanceResponseArray = rootObject.getJSONArray(Constants.KEY_ATTENDANCE_RESULT);
+
+                    for (int i = 0; i < attendanceResponseArray.length(); i++) {
+                        JSONObject attendanceResponseObject = (JSONObject) attendanceResponseArray.get(i);
+
+                        userId = attendanceResponseObject.getInt(Constants.KEY_USER_ID);
+
+                        JSONArray attendanceDetailArray = attendanceResponseObject.getJSONArray(Constants.KEY_ATTENDANCE_DETAIL);
+
+                        for (int j = 0; i < attendanceDetailArray.length(); i++) {
+
+                        }
+
+                        studentID = childResponseObject.getInt(Constants.KEY_STUDENT_ID);
+                        schoolID = childResponseObject.getInt(Constants.KEY_SCHOOL_ID);
+                        avrageAttendance = childResponseObject.getInt(Constants.KEY_AVG_ATTENDANCE);
+                        avragePerformance = childResponseObject.getInt(Constants.KEY_AVG_PERFORMANCE);
+                        studentFirstName = childResponseObject.getString(Constants.KEY_STUDENT_FIRST_NAME);
+
+                    }
+                }
+            } catch (Exception e) {
+                Log.e(Constants.TAG, "JSON PARSE ERROR: " + e);
+            }
+        }
     }
 }
